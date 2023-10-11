da27019b5366049aefdf2d0a841a088b6736cf01
zubeirarhesta <zubeirarhesta@gmail.com>
2023-10-11 16:31:58 +0700

    added footer section and its animation

---
+++ components/home-page/footer-section.js
@@ -0,0 +1,62 @@
+import styles from "@/components/home-page/footer-section.module.css";
+import Link from "next/link";
+import { forwardRef } from "react";
+
+const Footer = forwardRef(({ iconVisibility }, ref) => {
+  return (
+    <div ref={ref} className={styles.footer_container}>
+      <div className={styles.footer_title}>
+        <p>stacks used for this website</p>
+        <h3> html, css, js, react</h3>
+      </div>
+      <ul className={styles.footer_icons}>
+        <li
+          className={`${styles.hidden} ${
+            iconVisibility ? `${styles.show}` : ""
+          }`}
+        >
+          <Link href="https://www.facebook.com/zmarhesta">
+            <i className="fa fa-facebook"></i>
+          </Link>
+        </li>
+        <li
+          className={`${styles.hidden} ${
+            iconVisibility ? `${styles.show}` : ""
+          }`}
+        >
+          <p>|</p>
+        </li>
+        <li
+          className={`${styles.hidden} ${
+            iconVisibility ? `${styles.show}` : ""
+          }`}
+        >
+          <Link href="https://www.instagram.com/zmarhesta?igshid=MzNlNGNkZWQ4Mg==">
+            <i className="fa fa-instagram"></i>
+          </Link>
+        </li>
+        <li
+          className={`${styles.hidden} ${
+            iconVisibility ? `${styles.show}` : ""
+          }`}
+        >
+          <p>|</p>
+        </li>
+        <li
+          className={`${styles.hidden} ${
+            iconVisibility ? `${styles.show}` : ""
+          }`}
+        >
+          <Link href="https://www.linkedin.com/in/zubeir-muhammad-arhesta-41b5b210b/">
+            <i className="fa fa-linkedin"></i>
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.footer_copyright}>
+        © 2023 Zubeir Muhammad Arhesta
+      </div>
+    </div>
+  );
+});
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Footer from "@/components/home-page/footer-section";
 import Hero from "@/components/home-page/hero-section";
 import Photo from "@/components/home-page/photo-section";
 import Modal from "@/components/ui/modal";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function HomePage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentId, setCurrentId] = useState(null);
+
+  const footerRef = useRef(null);
+  const [isFooterIntersecting, setIsFooterIntersecting] = useState(false);
+  const [isIconVisible, setIsIconVisible] = useState(false);
 
   const openModal = (id) => {
     setIsModalOpen(true);
@@ -14,11 +19,50 @@ export default function HomePage() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    const footerObs = new IntersectionObserver(
+      ([entry]) => {
+        setIsFooterIntersecting(entry.isIntersecting);
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    footerObs.observe(footerRef.current);
+
+    return () => footerObs.unobserve(footerRef.current);
+  }, [isFooterIntersecting]);
+
+  useEffect(() => {
+    if (isFooterIntersecting) {
+      document.body.classList.add("colored_body");
+    } else {
+      document.body.classList.remove("colored_body");
+    }
+  }, [isFooterIntersecting]);
+
+  useEffect(() => {
+    const hiddenElements = footerRef.current.querySelectorAll("li");
+
+    hiddenElements.forEach((e) => {
+      if (isFooterIntersecting) {
+        /* e.classList.add("show"); */
+        setIsIconVisible(true);
+      } else {
+        // e.classList.remove("show");
+        setIsIconVisible(false);
+      }
+    });
+  }, [isFooterIntersecting]);
+
   return (
     <>
       <Hero />
       <Photo onPhotoClick={openModal} />
       <Modal id={currentId} onClose={closeModal} isOpen={isModalOpen} />
+      <Footer ref={footerRef} iconVisibility={isIconVisible} />
     </>
   );
 }
